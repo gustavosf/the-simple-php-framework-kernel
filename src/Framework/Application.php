46f0d2b81f4c7fc4cd636937e74113e9cf6db348
Gustavo Seganfredo <gustavosf@gmail.com>
2014-10-30 22:21:33 -0200

    Creating application core class

---
+++ src/Framework/Application.php
@@ -0,0 +1,110 @@
+<?php
+
+/**
+ * This file is part of The Simple PHP Framework
+ *
+ * @package Framework
+ * @author  Gustavo Seganfredo
+ */
+
+namespace Framework;
+
+/**
+ * Controller que gerencia assuntos relacionados às entrevistas de doutorado
+ * 
+ * @todo include other HTTP methods, as PUT, DELETE, etc, etc...
+ */
+class Application {
+
+	/**
+	 * Routes for this application
+	 * @var array
+	 */
+	protected $routes = ['get' => [], 'post' => []];
+
+	/**
+	 * Instance to a new application
+	 */
+	public function __construct() {}
+
+	/**
+	 * Maps a GET route
+	 * @param  string   $route    Matched route pattern
+	 * @param  function $function Controller
+	 * @return $this
+	 */
+	public function get($route, $function)
+	{
+		$this->routes['get'][$route] = $function;
+		return $this;
+	}
+
+	/**
+	 * Maps a POST route
+	 * @param  string   $route    Matched route pattern
+	 * @param  function $function Controller
+	 * @return $this
+	 */
+	public function post($route, $function)
+	{
+		$this->routes['post'][$route] = $function;
+		return $this;
+	}
+
+	/**
+	 * Handle a request
+	 * 
+	 * Find the related controller for a request, execute it or throw 404
+	 * exception in case of no route found
+	 * 
+	 * @param  Request $req Request to be handled
+	 * @return void
+	 * @throws HttpNotFoundException
+	 */
+	public function handle(Request $req)
+	{
+		if ($req->server->REQUEST_METHOD == 'GET') $routes = $this->routes['get'];
+		elseif ($req->server->REQUEST_METHOD == 'POST') $routes = $this->routes['post'];
+		else $routes = [];
+
+		$route = $this->matchRoute($req->server->PATH_INFO, $routes);
+
+		if ($route === null) throw new HttpNotFoundException;
+		return call_user_func_array($route[0], $route[1]);
+	}
+
+	/**
+	 * Tries to find a route for a given pathpattern, given a list of routes
+	 * 
+	 * @param  string $pattern Regex pattern
+	 * @param  array  $routes  List of available routes
+	 * @return array           [route, parameters]
+	 */
+	protected function matchRoute($path, array $routes)
+	{
+		foreach ($routes as $route_pattern => $route)
+		{
+			if (preg_match('@^'.$route_pattern.'$@', $path, $matches))
+			{
+				array_shift($matches);
+				return [$route, $matches];
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Runs the application on a server
+	 * 
+	 * @param  string $environment [description]
+	 * @return void
+	 */
+	public function run()
+	{
+		$request = Request::createFromServer();
+		return $this->handle($request);
+	}
+
+}
+
+class HttpNotFoundException extends \Exception {}
---
+++ tests/Framework/tests/ApplicationTest.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * This file is part of The Simple PHP Framework
+ *
+ * @package Framework
+ * @author  Gustavo Seganfredo
+ */
+
+namespace Framework\Tests;
+
+use Framework\Application;
+use Framework\Request;
+
+/**
+ * Application test cases.
+ */
+class ApplicationTest extends \PHPUnit_Framework_TestCase
+{
+
+	/**
+	 * @covers Framework\Application::get
+	 */
+	public function testGet()
+	{
+		$app = new Application();
+		$app->get('/', function() {
+			return 'ok';
+		});
+		$this->assertEquals('ok', $app->handle(Request::create('/')));
+	}
+
+	/**
+	 * @covers Framework\Application::post
+	 */
+	public function testPost()
+	{
+		$app = new Application();
+		$app->post('/', function() {
+			return 'ok';
+		});
+		$this->assertEquals('ok', $app->handle(Request::create('/', 'POST')));
+	}
+
+	/**
+	 * @covers Framework\Application::handle
+	 * @covers Framework\Application::matchRoute
+	 */
+	public function testHandle()
+	{
+		$app = new Application();
+		$app->get('/test/(\d+)', function($d) { return $d; });
+		$app->post('/test/(\d+)', function($d) { return $d; });
+		
+		$this->assertEquals('15', $app->handle(Request::create('/test/15')));
+		$this->assertEquals('15', $app->handle(Request::create('/test/15', 'POST')));
+
+		try {
+			$app->handle(Request::create('/'));
+			$this->assertTrue(false);
+		} catch (\Framework\HttpNotFoundException $e) {
+			$this->assertTrue(true);
+		}
+
+		try {
+			$app->handle(Request::create('/', 'PUT'));
+			$this->assertTrue(false);
+		} catch (\Framework\HttpNotFoundException $e) {
+			$this->assertTrue(true);
+		}
+
+	}
+
+}
